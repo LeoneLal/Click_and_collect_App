1976191363448990dd889be899747343007b4917
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-04-27 09:45:37 +0200

    Create Product Service and add API Call for products and categories

---
+++ ClickAndCollectApp/src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  
+  url = 'http://127.0.0.1:8000/api/';
+
+  constructor(private http: HttpClient) { }
+
+
+  getAllProducts() {
+    return this.http.get(`${this.url}products`);
+  }
+
+  getProductDetails(id) {
+    return this.http.get(`${this.url}products/${id}`);
+  }
+
+  getCategories() {
+    return this.http.get(`${this.url}category`);
+  }
+
+
+}
