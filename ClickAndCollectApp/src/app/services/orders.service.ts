06c23da4e663d8045b8aac379f64418c647ecd96
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-05-11 17:21:36 +0200

    Add order URL API

---
+++ ClickAndCollectApp/src/app/services/orders.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  url = 'http://127.0.0.1:8000/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getUserOrders(id) {
+    return this.http.get(`${this.url}orders/${id}`);
+  }
+
+  getOrderDetails(id) {
+    return this.http.get(`${this.url}orderlines/${id}`);
+  }
+}
