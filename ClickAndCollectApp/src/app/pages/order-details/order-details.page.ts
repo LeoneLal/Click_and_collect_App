bee65f5b60d37078b64bec4447e495024da46efe
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-05-11 17:23:46 +0200

    Create order-details.page.ts

---
+++ ClickAndCollectApp/src/app/pages/order-details/order-details.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrdersService } from 'src/app/services/orders.service';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-order-details',
+  templateUrl: './order-details.page.html',
+  styleUrls: ['./order-details.page.scss'],
+})
+export class OrderDetailsPage implements OnInit {
+
+  orderDetails: any;
+  productDetails: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private ordersService: OrdersService, private productService: ProductService) { }
+
+  ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id')
+
+    this.ordersService.getOrderDetails(id).subscribe(res => {
+      this.orderDetails = res
+      console.log(this.orderDetails);
+
+      this.orderDetails.forEach(line => {
+        this.productService.getProductDetails(line.product_id).subscribe(result => {
+          line.name = result["name"]
+          line.picture_slug = result["picture_slug"]
+        });
+      });
+    });
+
+    console.log(this.orderDetails)
+  }
+
+}
+
+
