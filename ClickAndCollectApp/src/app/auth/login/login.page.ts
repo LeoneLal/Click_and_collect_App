5a69fa7859a2bb1b90304602d4f9663bb869335b
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-05-11 08:21:04 +0200

    Add login page and function

---
+++ ClickAndCollectApp/src/app/auth/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+
+  ngOnInit() {
+  }
+
+  login(form) {
+    console.log(form)
+    this.authService.login(form.value).subscribe((res) => {
+      console.log(JSON.stringify(res["user"]));
+      this.router.navigateByUrl('/');
+      sessionStorage.setItem("authToken", res["access_token"]);
+      sessionStorage.setItem("user", JSON.stringify(res["user"]));
+    });
+  }
+
+}
