f61eda41c346787dcfd6afd46e988f5442c984a9
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-05-11 08:16:50 +0200

    Add authentication API Url

---
+++ ClickAndCollectApp/src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  url = 'http://127.0.0.1:8000/api/auth/';
+
+  constructor(private http: HttpClient) { }
+
+
+  register(form) {
+    return this.http.post(`${this.url}register`, form);
+  }
+
+  login(form) {
+    return this.http.post(`${this.url}login`, form);
+  }
+
+  logout() {
+    let form = "";
+    return this.http.post(`${this.url}logout`, form);
+  }
+}
