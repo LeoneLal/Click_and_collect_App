b190118c30da6f327c99deddf48174d975a188f2
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-05-17 10:20:56 +0200

    Update cart.service.ts

--- ClickAndCollectApp/src/app/services/cart.service.ts
+++ ClickAndCollectApp/src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
- 
+
 export interface Product {
   id: number;
   name: string;
@@ -12,25 +13,29 @@ export interface Product {
   providedIn: 'root'
 })
 export class CartService {
+
+  constructor() { }
+
+
+
   data: Product[] = [];
- 
+
   private cart = [];
   private cartItemCount = new BehaviorSubject(0);
- 
-  constructor() {}
- 
+
+
   getProducts() {
     return this.data;
   }
- 
+
   getCart() {
     return this.cart;
   }
- 
+
   getCartItemCount() {
     return this.cartItemCount;
   }
-  
+
   // Increment quantity
   addProduct(product) {
     let added = false;
@@ -47,7 +52,7 @@ export class CartService {
     }
     this.cartItemCount.next(this.cartItemCount.value + 1);
   }
- 
+
   // Decrement quantity
   decreaseProduct(product) {
     for (let [index, p] of this.cart.entries()) {
@@ -60,7 +65,7 @@ export class CartService {
     }
     this.cartItemCount.next(this.cartItemCount.value - 1);
   }
- 
+
   // Remove from cart
   removeProduct(product) {
     for (let [index, p] of this.cart.entries()) {
